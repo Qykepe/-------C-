fc93511f7006dda009a746ef1153593c9c4f24e6
Qykepe <140060537+Qykepe@users.noreply.github.com>
2023-07-25 17:05:00 +0300

    Сделал дз

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите число:");
+int number = int.Parse(Console.ReadLine()!);
+int num = number%2;
+int number2 = 1;
+while (number > number2)
+{
+    Console.WriteLine($"{num}");
+    number--;
+}
