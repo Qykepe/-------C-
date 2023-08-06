9f7ae1c5b694199270c590011fd820ab39974108
Qykepe <140060537+Qykepe@users.noreply.github.com>
2023-08-06 11:12:17 +0300

    Ещё одно дз позади

--- Program.cs
+++ Program.cs
@@ -1,7 +1,2 @@
-﻿Console.Write("Введите число N:");
-int number = int.Parse(Console.ReadLine()!);
-int index = 1;
-while (index <= number){
-    Console.WriteLine(Math.Pow(index, 3));
-    index ++;
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
