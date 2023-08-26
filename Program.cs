2f7031877acf7d6530d53f85998b58f57f2a58a6
Qykepe <140060537+Qykepe@users.noreply.github.com>
2023-08-27 00:43:50 +0300

    50 задание

--- Program.cs
+++ Program.cs
@@ -1,14 +1,65 @@
-﻿Console.Write("Введите угловой коэффициент k1:");
-double k1 = Convert.ToInt32(Console.ReadLine()!);
-Console.Write("Введитt свободный коэффициент b1:");
-double b1 = Convert.ToInt32(Console.ReadLine()!);
-Console.Write("Введите угловой коэффициент k2:");
-double k2 = Convert.ToInt32(Console.ReadLine()!);
-Console.Write("Введитt свободный коэффициент b2:");
-double b2 = Convert.ToInt32(Console.ReadLine()!);
+﻿// Console.WriteLine("Введите количество строк массива:");
+// int rows = int.Parse(Console.ReadLine()!);
+
+// Console.WriteLine("Введите количество столбцов массива:");
+// int colums = int.Parse(Console.ReadLine()!);
+
+// int[,] array = GetArray(rows, colums, 0, 10);
+// PrintArray(array);
+
+// //Создание массива
+// int[,] GetArray(int m, int n, int MinValue, int MaxValue)
+// {
+//     int[,] res = new int[m,n];
+//     for(int i = 0; i < m; i++){
+//         for(int j = 0; j < n; j++){
+//             res[i,j] = new Random().Next(MinValue, MaxValue + 1);
+//         }
+//     }
+//     return res;
+// }
+// //Печать массива
+// void PrintArray(int[,] array)
+// {
+//     for(int i = 0; i < array.GetLength(0); i++){
+//         for(int j = 0; j < array.GetLength(1); j++){
+//             Console.Write  ($"{array[i,j]} ");
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+Console.WriteLine("Введите количество строк:");
+int rows = Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("Введите количество строк:");
+int colums = Convert.ToInt32(Console.ReadLine()!);
+
+double[,] array = new double[rows, colums];
+
+GetArray(array);
+PrintArray(array);
+
+//Массив
+void GetArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++){
+        for(int j = 0; j < array.GetLength(1); j++){
+            array[i,j] = new Random().Next(-99,99) / 100.0;
+        }
+    }
+}
+
+//Печать массива
+void PrintArray(double[,] array)
+{
+       for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
 
 
-double x = (b1 - b2)/(k2-k1);
-double y = k1 * x + b1;
-
-Console.WriteLine($"Две прямые пересекутся в точке с координатами OX: {x}, OY: {y}");
